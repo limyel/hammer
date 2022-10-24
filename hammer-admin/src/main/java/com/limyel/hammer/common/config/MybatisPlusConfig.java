ca2e6c67fb777923bd3ab78166d4797d7d3766e1
limyel <limyel@outlook.com>
2022-10-24 14:31:15 +0800

    MybatisPlus配置类

---
+++ hammer-admin/src/main/java/com/limyel/hammer/common/config/MybatisPlusConfig.java
@@ -0,0 +1,21 @@
+package com.limyel.hammer.common.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+
+        // 分页插件
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+
+        return mybatisPlusInterceptor;
+    }
+
+}
